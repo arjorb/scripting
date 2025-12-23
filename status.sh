a225f5d15b34819b93babe633047550d3602381e
arjorb <arjorb@gmail.com>
2025-12-23 10:34:49 +0200

    script to return the current server status usage

--- status.sh
+++ status.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#Script that returns the current server status.
+server_name=$(hostname)
+
+function memoryCheck(){
+    echo ""
+        echo "The current memory usage on ${server_name} is: "
+        echo ""
+        top -l 1 -s 0 | head -n 10
+        echo ""
+}
+memoryCheck
