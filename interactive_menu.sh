f8a698231b97e4b94ec59a97ff898d659dec1a6f
arjorb <arjorb@gmail.com>
2025-12-23 10:53:11 +0200

    adding colors

--- interactive_menu.sh
+++ interactive_menu.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+##
+# Color Variables
+##
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+
+##
+##
+# Color Functions
+ColorGreen(){
+    echo -ne $green$1$clear
+    }
+ColorBlue(){
+    echo -ne $blue$1$clear
+}
+
